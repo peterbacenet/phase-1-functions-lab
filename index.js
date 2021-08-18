68177cc6e7013b1a4315d95b46b4d0845c2e704c
Peter Bacenet <peterbacenet@icloud.com>
2021-08-17 21:18:07 -0400

    commit tree d914cd4d9afd276f1d8af9df1fba3d4825675764

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+let farePrice;
+
+function distanceFromHqInBlocks(location) {
+    let distance = Math.abs(location-42)
+    return (distance)
+}
+function distanceFromHqInFeet(location) {
+    let distanceinFeet = Math.abs((location-42)*264)
+    return distanceinFeet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distanceTravelledinFeet = Math.abs((start-destination)*264)
+    return distanceTravelledinFeet
+}
+
+
+
+function calculatesFarePrice(start,destination) {
+    const test = distanceTravelledInFeet(start,destination);
+    if (test <= 400) {
+        farePrice = 0
+        return farePrice;
+    }
+    else if (test <= 2000) {
+        return farePrice = (test-400)* .02
+    }
+    else if (test <= 2500) {
+        return farePrice = 25
+    }
+    else { return 'cannot travel that far'}
+    }
+
+
+
+
+// function calculatesFarePrice(distanceTravelledInFeet) {
+//     if (distance > 2500) {
+// response = 'No can do.'
+// }
+//     else if (distance > 2000) {
+// response = 'I will gladly take your 25 dollars.'
+// }
+//     else if (distance > 400) { 
+// response = 'It is 2 cents per mile'
+// };
+//     return response;
+// }
+
+
+//free sample?
+
+
+// navigate the last question? 
